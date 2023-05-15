ff4adc54d92637744290daa009e1453ad2b648b9
Julien Sulpis <julien.sulpis@gmail.com>
2023-05-15 20:13:44 +0200

    feat: setup template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,17 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/slides-template",
+	base: "/slides-template",
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: (source, path) =>
+						path.endsWith("abstract.scss")
+							? source
+							: `@use "src/styles/_abstract" as *; ${source}`,
+				},
+			},
+		},
+	},
 });
