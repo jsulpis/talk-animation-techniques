f6b8b705d7d8211c7ee37cf27a37f9b4163d1075
Julien Sulpis <julien.sulpis@gmail.com>
2023-05-22 22:08:01 +0200

    refactor: move slides of part 1

---
+++ src/slides/part-1/css/transition/TransitionGraph.svelte
@@ -0,0 +1,18 @@
+<div class="mermaid">
+	<pre>
+flowchart LR
+  subgraph TOP
+    direction TB
+    subgraph B1
+        direction RL
+        i1 -->f1
+    end
+    subgraph B2
+        direction BT
+        i2 -->f2
+    end
+  end
+  A --> TOP --> B
+  B1 --> B2
+	</pre>
+</div>
