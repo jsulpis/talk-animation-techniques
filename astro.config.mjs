0220d848e5164325eb127f59530eb430ff021e77
Julien Sulpis <julien.sulpis@gmail.com>
2023-06-16 23:37:42 +0200

    chore: deploy with github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import glsl from "vite-plugin-glsl";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://jsulpis.github.io",
 	base: "/talk-animation-techniques",
 	integrations: [svelte(), vue(), react()],
 	vite: {
